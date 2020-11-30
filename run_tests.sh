79a1d3a70f93cf2452fa2242c9306207a8153882
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2020-11-30 13:06:36 +0000

    PLATUI-832 - Updating test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -e
-ENV=${1:-local}
-BROWSER=${2:-chrome}
-DRIVER=
 
-if [ "$BROWSER" = "chrome" ]; then
-    DRIVER="-Dwebdriver.chrome.driver=/usr/local/bin/chromedriver"
-elif [ "$BROWSER" = "firefox" ]; then
-    DRIVER="-Dwebdriver.gecko.driver=/usr/local/bin/geckodriver"
+BROWSER_TYPE=$1
+ENV=$2
+
+if [ -z "$BROWSER_TYPE" ]; then
+    echo "BROWSER_TYPE value not set. This should be passed as a parameter"
+    exit 1
 fi
 
 # Scalafmt checks have been separated from the test command to avoid OutOfMemoryError in Jenkins
 sbt scalafmtCheckAll scalafmtSbtCheck
 
-sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER "testOnly uk.gov.hmrc.test.ui.specs.*"
+sbt -Dbrowser=${BROWSER_TYPE} -Denv=${ENV:=local} "testOnly uk.gov.hmrc.test.ui.specs.*"
